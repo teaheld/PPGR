e95559da5c74d3bf07b9c662706e930a135ff805
teaheld <heldrich013@gmail.com>
2019-11-19 21:04:04 +0100

    Dodato izracunavanje jedinicnog vektora i ugla rotacije

--- Drugi domaci/domaci2.py
+++ Drugi domaci/domaci2.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math
 
+np.set_printoptions(precision=10, suppress=True)
+
+
 def Euler2A(fi, teta, psi):
     '''
             Forumule preuzete sa prezentacije,
@@ -24,8 +27,55 @@ def Euler2A(fi, teta, psi):
     '''
     return np.matmul(np.matmul(Rz, Ry), Rx)
 
+
+from numpy import linalg as la
+def AxisAngle(A):
+
+    '''
+            Odredjujemo sopstvene vrednosti i vektore.
+    '''
+    w, v = la.eig(np.array(A))
+    '''
+            Odrediti jedinicni sopstveni vektor p
+            za lambda = 1.
+    '''
+    '''
+            Nalazimo lambda = 1.
+    '''
+    i_lambda = np.where(w.real.round() == 1)[0][0]
+    p = v[:, i_lambda].real
+
+    '''
+            Odrediti proizvoljan jedinicni vektor u _|_ p.
+            
+            Posto je matrica ulaz matrica A koja je simetricna,
+            Sopstveni vektori su normalni jedni na druge, 
+            pa cemo uzeti proizvoljan sopstveni vektor koji nismo iskoristili.
+    '''
+    u = v[:, len(p) - i_lambda -1].real
+
+    '''
+            u' = Au,
+            gde je u' jedinicni.
+    '''
+    u_p = np.dot(A, u)
+    u_p = u_p / la.norm(u_p, 2)
+
+    '''
+            fi = arccos(u, u')
+    '''
+    fi = math.degrees(math.acos(np.dot(u, u_p)))
+
+    '''
+            Vraca jedinicni vektor p 
+            i ugao fi 
+            tako da A = Rp(fi).
+    '''
+    return (p, fi)
+
 def main():
-    print(Euler2A(-np.arctan(1/4), -np.arcsin(8/9), np.arctan(4)))
+    A = Euler2A(-np.arctan(1/4), -np.arcsin(8/9), np.arctan(4))
+    print(AxisAngle(A))
 
 if __name__ == '__main__':
 	main()
