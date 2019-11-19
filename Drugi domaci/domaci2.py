7cd31061689a00902f8fdcbcb774d82472b8f259
teaheld <heldrich013@gmail.com>
2019-11-20 00:43:44 +0100

    Dodato jos 3 fje

--- Drugi domaci/domaci2.py
+++ Drugi domaci/domaci2.py
@@ -64,7 +64,15 @@ def AxisAngle(A):
     '''
             fi = arccos(u, u')
     '''
-    fi = math.degrees(math.acos(np.dot(u, u_p)))
+    fi = math.acos(np.dot(u, u_p))
+
+    '''
+            Da bi rotacija bila u pozitivnom smeru,
+            ako je [u, u', p] < 0
+            p = -p. 
+    '''
+    if (np.cross(u, u_p)).dot(p):
+        p = -p
 
     '''
             Vraca jedinicni vektor p 
@@ -73,9 +81,57 @@ def AxisAngle(A):
     '''
     return (p, fi)
 
+def Rodriqez(p, fi):
+    '''
+            Formule preuzete sa prezentacije, strana 7.
+    '''
+    ppt = np.matmul(p[np.newaxis].transpose(), p[np.newaxis])
+    px = np.matrix([[0, -p[2], p[1]],
+          [p[2], 0, -p[0]],
+          [-p[1], p[0], 0]])
+
+    return ppt + math.cos(fi) * (np.eye(3) - ppt) + math.sin(fi) * px
+
+def A2Euler(A):
+    '''
+            Slajdovi, strana 22.
+    '''
+    fi = 0
+    teta = math.pi / 2
+    if A[2][0] < 1:
+        if A[2][0] > -1:
+            '''
+                    Jedinstveno resenje.
+            '''
+            psi = math.atan2(A[1][0], A[0][0])
+            teta = math.asin(-A[2][0])
+            fi = math.atan2(A[2][1], A[2][2])
+        else:
+            '''
+                    Nije jedinstveno. 
+            '''
+            psi = math.atan2(-A[0][1], A[1][1])
+    else:
+        psi = math.atan2(-A[0][1], A[1][1])
+
+    return (fi, teta, psi)
+
+def AxisAngle2Q(p, fi):
+    '''
+            Slajdovi, strana 34.
+    '''
+    w = math.cos(fi / 2)
+    p = p / la.norm(p)
+    x, y, z = math.sin(fi / 2) * p
+
+    return (x, y, z, w)
+
 def main():
     A = Euler2A(-np.arctan(1/4), -np.arcsin(8/9), np.arctan(4))
-    print(AxisAngle(A))
+    p, fi = AxisAngle(A)
+    Rp = Rodriqez(p, fi)
+    E_fi, E_teta, E_psi = A2Euler(A)
+    quaternions = AxisAngle2Q(p, fi)
 
 if __name__ == '__main__':
 	main()
